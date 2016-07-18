aeedd171b8e264a096ccdd43863769b03578d5c7
Dave Strus <dave@getfretless.com>
2016-07-18 11:00:42 -0400

    Create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new post_params
+    post.save
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link_url, :body)
+  end
 end
