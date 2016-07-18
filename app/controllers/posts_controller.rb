e4f8ff28b885da6315e52dac97a0ce6afbc7e422
Dave Strus <dave@getfretless.com>
2016-07-18 15:42:27 -0400

    Validate that post#title is present and under 255 characters.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new post_params
-    post.save
-    redirect_to posts_path
+    @post = Post.new post_params
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   private
