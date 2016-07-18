d96d4f0b72bd3da85434b4d219af3cdd921da01d
Dave Strus <dave@getfretless.com>
2016-07-18 15:54:02 -0400

    Display flash messages upon creating posts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def flash_messages(flash)
+    flash.map do |name, msg|
+      content_tag :div, class: "alert #{name}" do
+        flash_message_output msg
+      end
+    end.join.html_safe
+  end
+
+  def flash_message_output(content)
+    if content.is_a? Array
+      flash_list content
+    else
+      content
+    end
+  end
+
+  def flash_list(messages)
+    content_tag :ul do
+      messages.map do |message|
+        content_tag(:li, message)
+      end.join.html_safe
+    end
+  end
 end
