d057ef0488af4f49529a7d3e87be68267c9bbb3a
Dave Strus <dave@getfretless.com>
2016-07-19 15:24:06 -0400

    Create categories.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,30 @@
+class CategoriesController < ApplicationController
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to posts_path, flash: { :"alert-success" => "Thank you for adding a subbluit." } }
+        format.json { render :show, status: :created, location: @category }
+      else
+        flash.now[:"alert-danger"] = @category.errors.full_messages
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :title, :description, :sidebar, :submission_text)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :categories
+  
   root 'posts#index'
 end
