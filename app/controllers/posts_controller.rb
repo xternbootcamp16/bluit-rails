410772f8af43260de299f783b0f51a63e56c7ee4
Dave Strus <dave@getfretless.com>
2016-07-19 15:54:03 -0400

    Select a category from the post form.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :link_url, :body)
+      params.require(:post).permit(:title, :link_url, :body, :category_id)
     end
 end
