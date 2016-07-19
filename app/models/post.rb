247ba5a423d5aaabb35d3c46c8321a9108d61a6a
Dave Strus <dave@getfretless.com>
2016-07-19 12:05:37 -0400

    Add relation between category and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
   validates :title, length: { maximum: 255 }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719152900) do
+ActiveRecord::Schema.define(version: 20160719155310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,15 @@ ActiveRecord::Schema.define(version: 20160719152900) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.string   "link_url",   limit: 2000
+    t.string   "title",       limit: 255
+    t.string   "link_url",    limit: 2000
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
+
+  add_foreign_key "posts", "categories"
 end
