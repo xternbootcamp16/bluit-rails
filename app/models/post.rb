3c2d10e59545910109be757da29df09465e8034c
Dave Strus <dave@getfretless.com>
2016-07-20 11:14:07 -0400

    Display category names on posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :category
   validates :title, length: { maximum: 255 }, presence: true
 
-  default_scope { order(updated_at: :desc) }
+  default_scope { order(updated_at: :desc).includes(:category) }
 end
