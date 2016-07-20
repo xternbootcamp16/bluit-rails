94971a83ee08c1390417bf9ef38d408399a42513
Dave Strus <dave@getfretless.com>
2016-07-20 10:42:35 -0400

    Use a helper method to load categories for use in views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  helper_method :categories
+
+  private
+
+  def categories
+    @categories = Category.all if !defined? @categories
+    @categories
+  end
 end
