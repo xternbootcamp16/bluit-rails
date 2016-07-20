d5c1475e2c1e35da46152fae9efacaeba054c35f
Dave Strus <dave@getfretless.com>
2016-07-20 15:28:45 -0400

    Sort categories alphabetically, and posts chronologically.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.includes(:category).all
+    @posts = Post.all
   end
 
   # GET /posts/1
